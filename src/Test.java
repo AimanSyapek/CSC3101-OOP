9f348211ef73a79d1b3d9eedfa4cc055f45000ca
Shiro <210564@student.upm.edu.my>
2022-03-22 08:22:24 +0800

    init

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test{ 
+public static void main(String[] args) {
+    Test csc3101 = new Test();
+}
+}
+
+public class Test{
+    Test(){
+        System.out.println("Hello, World!");
+    }
+}
